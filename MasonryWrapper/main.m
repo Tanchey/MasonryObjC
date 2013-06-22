2431e8f2d78242857413b2ebb3c5da2cf8822bba
Tatyana Lomonosova <Tatjana.Lomonosova@noveogroup.com>
2013-06-22 15:37:32 +0700

    Initial Commit

---
+++ MasonryWrapper/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  MasonryWrapper
+//
+//  Created by Tatyana Lomonosova on 6/22/13.
+//  Copyright (c) 2013 Tatyana Lomonosova. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
